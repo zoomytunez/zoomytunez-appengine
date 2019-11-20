756bdb3d8b16e1f201971411525c26de8f126456
Jordan Yoon-Buck <yoonbuck@uw.edu>
2019-11-20 01:39:10 -0800

    Add link for spotify login

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write("Hello, world!")
         self.response.write("<br>Secret variable: ")
         self.response.write(os.getenv("SECRET"))
-        self.response.write('<br><a href="test.html">static</a>')
+        self.response.write('<br><a href="test.html">static asset test</a>')
+        self.response.write('<br><a href="/auth/login/spotify">login with spotify</a> ')
+        self.response.write('<a href="/auth/login/spotify?force=true">(force dialog)</a>')
 
 application = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
