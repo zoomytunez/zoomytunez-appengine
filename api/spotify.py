1f92b64449c6d108cd9b565c6ae66ea943607521
Vishaka Nirmal <vnirmal@uw.edu>
2019-11-23 17:22:06 -0800

    worked on spotify api req

--- api/spotify.py
+++ api/spotify.py
@@ -1,11 +1,20 @@
 from util import safeGet, pretty
+import json, base64
+from os import getenv
+
+URL = "https://api.spotify.com/v1/"
 
 def getNewAcessToken(refreshToken):
-    pass
+    b64string = base64.b64encode("%s:%s" % (getenv('SPOTIFY_ID'), getenv('SPOTIFY_SECRET')))
+    auth = "Basic " + b64string
+    res = safeGet("https://accounts.spotify.com/api/token", data={
+        "grant_type": "refresh_token",
+        "refresh_token": refreshToken
+    }, auth= auth)
+    data = json.load(res)
+    return data["access_token"]
 
 def getUserInformation(accessToken, refreshToken):
-
     def errorHandler():
         pass
-
-    safeGet("spotify/whatever", error=errorHandler)
+    safeGet(URL + "me", auth= authToken,  error=errorHandler)
