f7c7fb747eb4384676a7029d5e0416ac6ae8ffa7
Jordan Yoon-Buck <yoonbuck@uw.edu>
2019-12-09 15:30:49 -0800

    Add CORS support

--- routes/zoomyapi.py
+++ routes/zoomyapi.py
@@ -1,15 +1,14 @@
 import webapp2, urllib
 from model import User
+from util.cors import requiresAuth, checkOrigin, CORSRequestHandler
 
-class SpotifySearch(webapp2.RequestHandler):
-    def get(self):
+class SpotifySearch(CORSRequestHandler):
+    @checkOrigin
+    @requiresAuth(spotify=True)
+    def get(self, spotifyAPI):
         params = self.request.GET
         if "q" not in params:
             self.response.status = 400
-            return
-        spotifyAPI = User.getSpotifyForSession(self.request)
-        if not spotifyAPI:
-            self.response.status = 401
             return
         limit = params["limit"] if "limit" in params else 20
         offset = params["offset"] if "offset" in params else 0
@@ -22,16 +21,13 @@ class SpotifySearch(webapp2.RequestHandler):
         self.response.headers.add("Content-Type", "application/json")
         self.response.write(searchResults)
 
-class SpotifyGenres(webapp2.RequestHandler):
-    def get(self):
-        spotifyAPI = User.getSpotifyForSession(self.request)
-        if not spotifyAPI:
-            self.response.status = 401
-            return
+class SpotifyGenres(CORSRequestHandler):
+    @checkOrigin
+    @requiresAuth(spotify=True)
+    def get(self, spotifyAPI):
         searchResults = spotifyAPI.getGenres(parse=False)
         self.response.headers.add("Content-Type", "application/json")
         self.response.write(searchResults)
-
 
 route = webapp2.WSGIApplication([
     ("/api/search", SpotifySearch),
---
+++ util/cors.py
@@ -0,0 +1,77 @@
+import webapp2
+from model import User
+
+def requiresAuth(spotify=False, strava=False):
+    def decorator(func):
+        def wrapper(self):
+            kwargs = {}
+            session = None
+            if spotify or strava:
+                user = User.getForSession(self.request)
+            if spotify:
+                try:
+                    spotifyAPI = user.getSpotifyAPI()
+                    if not spotifyAPI: raise Error()
+                except:
+                    self.response.status = 401
+                    self.response.write("Unauthorized")
+                    return
+                kwargs["spotifyAPI"] = spotifyAPI
+            if strava:
+                # stravaAPI = user.getStravaAPI()
+                # if not stravaAPI:
+                    # self.response.status = 401
+                    # self.response.write("Unauthorized")
+                    # return
+                # kwargs["stravaAPI"] = stravaAPI
+                pass # need to implement strava api
+            func(self, **kwargs)
+        return wrapper
+    return decorator
+
+
+ACCEPTABLE_REFERERS = ["localhost", "zoomy-tunez-fb.web.app", "zoomy-tunez-fb.firebaseapp.com", "zoomytunez.appspot.com"]
+
+def _originTest(origin):
+    parts = origin.split(":")
+    if parts[0] == "http" and parts[1] != "//localhost":
+        return False # don't allow http requests if not local
+    return parts[1][2:] in ACCEPTABLE_REFERERS
+
+def checkOrigin(func):
+    def wrapper(self):
+        if "Origin" in self.request.headers:
+            origin = self.request.headers["Origin"]
+            if not _originTest(origin):
+                self.response.status = 403
+            self.response.headers.add("Access-Control-Allow-Origin", origin)
+            self.response.headers.add("Vary", "Origin")
+            self.response.headers.add("Access-Control-Allow-Credentials", "true")
+            if func.__name__ == "options" and self.CORS_METHODS:
+                self.response.headers.add("Access-Control-Allow-Methods", self.CORS_METHODS)
+        func(self)
+    return wrapper
+
+class CORSRequestHandler(webapp2.RequestHandler):
+
+    CORS_METHODS = "GET"
+
+    @checkOrigin
+    def options(self):
+        pass
+
+# Usage:
+#
+# class Handler(CORSRequestHandler):
+#
+#     CORS_METHODS = "GET, POST"
+#
+#     @checkOrigin
+#     @requiresAuth(spotify=True)
+#     def get(self, spotifyAPI):
+#         ...
+#
+#     @checkOrigin
+#     @requiresAuth(spotify=True)
+#     def post(self, spotifyAPI):
+#         ...
